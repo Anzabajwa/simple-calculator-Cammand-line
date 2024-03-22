4bf98e493c93c785b8e339b21cb11962ff6b9b36
ANZA BAJWA <161083667+Anzabajwa@users.noreply.github.com>
2024-03-22 06:02:03 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env  node
+
+import inquirer from "inquirer" ;
+
+
+const answer = await  inquirer.prompt([
+    { massage:"enter first number" , type : "number" , name:"firstNumber"},
+   {  massage: "enter your second number", type : "number" , name:"secondNumber"},
+
+
+   { 
+    massage : "Select one of the operators to perform action " , 
+    type : "list" ,
+    name: "operator",
+     choices:["Addition" , "Substraction" , "Multiplication" , "Division"],
+    },
+
+
+    
+]);
+
+//conditional statement 
+if(answer.operator === "Addition" ){
+    console.log( answer.firstNumber + answer.secondNumber);
+
+}else if (answer.operator === "Substraction" ) {
+    console.log(answer.firstNumber - answer.secondNumber);
+
+}else if (answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber );
+
+}else if (answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+} else{
+    console.log("please select a valid operator");
+}
+
+
